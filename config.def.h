afb737de9d3a7f44a170b2af443d0ceaacf0f986
chandrahmuki <amouyaljerome@gmail.com>
2019-09-23 09:47:09 +0200

    config

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "hack:size=15" };
-//#include "/home/david/.cache/wal/colors-wal-dwm.h"
+static const char *fonts[]          = { "Hack:size=15" };
+#include "colors-wal-dwm.h"
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
