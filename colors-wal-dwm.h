e12966611adb0b352d1218c3767b00acfc48d47f
chandrahmuki <amouyaljerome@gmail.com>
2019-09-23 23:33:29 +0200

    colors

--- colors-wal-dwm.h
+++ colors-wal-dwm.h
@@ -1,14 +1,14 @@
-static const char norm_fg[] = "#9ebbcf";
-static const char norm_bg[] = "#161603";
-static const char norm_border[] = "#6e8290";
+static const char norm_fg[] = "#aeebf1";
+static const char norm_bg[] = "#0b071d";
+static const char norm_border[] = "#79a4a8";
 
-static const char sel_fg[] = "#9ebbcf";
-static const char sel_bg[] = "#0779B0";
-static const char sel_border[] = "#9ebbcf";
+static const char sel_fg[] = "#aeebf1";
+static const char sel_bg[] = "#285594";
+static const char sel_border[] = "#aeebf1";
 
-static const char urg_fg[] = "#9ebbcf";
-static const char urg_bg[] = "#306F99";
-static const char urg_border[] = "#306F99";
+static const char urg_fg[] = "#aeebf1";
+static const char urg_bg[] = "#68258F";
+static const char urg_border[] = "#68258F";
 
 static const char *colors[][3]      = {
     /*               fg           bg         border                         */
