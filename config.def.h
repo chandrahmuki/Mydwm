ae4e6ea829a952b6e51a822eeb88839941052b78
chandrahmuki <amouyaljerome@gmail.com>
2019-09-23 23:12:17 +0200

    heart

--- config.def.h
+++ config.def.h
@@ -10,10 +10,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=15" };
+static const char *fonts[]          = { "hack:size=18" };
 #include "colors-wal-dwm.h"
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
